ee785f543ba15279ab1c7b64ab92be89ee3693d8
Kiwa98 <144538771+Kiwa98@users.noreply.github.com>
2024-01-22 22:18:31 -0300

    Add files via upload

---
+++ ProjetoHospedagem/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+using DesafioProjetoHospedagem;
+using DesafioProjetoHospedagem.Models;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+
+List<Pessoa> hospedes = new List<Pessoa>();
+
+Pessoa p1 = new Pessoa(nome: "Hóspede 1");
+Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+Pessoa p3 = new Pessoa(nome: "Hóspede 3");
+
+hospedes.Add(p1);
+hospedes.Add(p2);
+//hospedes.Add(p3);
+
+Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
+
+
+Reserva reserva = new Reserva(diasReservados: 9);
+reserva.CadastrarSuite(suite);
+reserva.CadastrarHospedes(hospedes);
+
+
+Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
+Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
